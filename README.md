1e952c3eddecca7944f0cb4293db7c21fbeaa14b
guenter <kixi73@gmail.com>
2017-07-15 17:14:18 +0200

    final

--- README.md
+++ README.md
@@ -1,36 +1,19 @@
 # study
 
-FIXME: description
+My solution in Clojure to
+http://norvig.com/java-lisp.html
 
-## Installation
-
-Download from http://example.com/FIXME.
 
 ## Usage
 
 FIXME: explanation
 
-    $ java -jar study-0.1.0-standalone.jar [args]
+    $ java -jar study-0.1.0-standalone.jar 
 
-## Options
-
-FIXME: listing of options this app accepts.
-
-## Examples
-
-...
-
-### Bugs
-
-...
-
-### Any Other Sections
-### That You Think
-### Might be Useful
 
 ## License
 
-Copyright © 2017 FIXME
+Copyright © 2017 Guenter Kickinger
 
 Distributed under the Eclipse Public License either version 1.0 or (at
 your option) any later version.
--- src/study/core.clj
+++ src/study/core.clj
@@ -2,73 +2,77 @@
   (:gen-class))
 
 (def trans {\E 0
-            \J 1
-            \N 1
-            \Q 1
-            \R 2
-            \W 2
-            \X 2
-            \D 3
-            \S  3
-            \F 4
-            \T  4
-            \A 5
-            \M  5
-            \C 6
-            \I  6
-            \V 6
-            \B  7
-            \K 7
-            \U  7
-            \L 8
-            \O  8
-            \P 8
-            \G  9
-            \H 9
-            \Z  9})
+            \J 1 \N 1 \Q 1
+            \R 2 \W 2 \X 2
+            \D 3 \S  3
+            \F 4 \T  4
+            \A 5 \M  5
+            \C 6 \I  6 \V 6
+            \B  7 \K 7 \U  7
+            \L 8 \O  8 \P 8
+            \G  9 \H 9 \Z  9})
 
-(def dictionary (with-open [rdr (clojure.java.io/reader "/home/guenter/prog/study/resource/dictionary.txt")]
-                  (into [] (line-seq rdr))))
+(def dictionary
+  (with-open [rdr (clojure.java.io/reader
+                   (clojure.java.io/resource "dictionary.txt"))]
+    (into [] (line-seq rdr))))
 
-(take 10 dictionary)
 
-(defn map-word [word]
-  (filter #(not= % nil) (map #(trans %) (.toUpperCase word))))
+(defn word-to-digits [word]
+  (->> (.toUpperCase word)
+       (map #(trans %) )
+       (filter #(not= % nil) )))
 
-(map-word "hell\"o")
 
 (def mapped-dictionary
-  (into {} (group-by (fn [[code word]] code) (map (fn [x] [(map-word x) x]) dictionary))))
-
-(take 2 mapped-dictionary)
-(mapped-dictionary '(3 4 0 8 8 0 1 9 0 3 7 6 9))
+  (->> dictionary
+       (map (fn [x] [(word-to-digits x) x]) )
+       (group-by (fn [[code word]] code) )
+       (into {} )))
 
 (defn to-number-array [number]
-  (filter #(and (> % 0) (< % 10)) (map #(- (int %) (int \0)) number)))
+  (->> number
+       (map #(- (int %) (int \0)) )
+       (filter #(and (>= % 0) (< % 10)))))
 
-(defn find-match [number-array]
-  (if (= (count number-array) 1)
-    number-array
-    (if (= (count number-array) 0)
-      []
-      (for [r (range 2 (inc (count number-array)))]
-        (let [[curr next] (split-at r number-array)
-              _ (println "range: " curr)
-              fs (mapped-dictionary curr)
-              ns (find-match next)]
-          (for [[_ f] fs
-                n ns]
-            [f n]
-            )
-          )))))
 
-(find-match [1 0 1 0 3 1 0 0 3] )
-(range 2 10)
-(mapped-dictionary [1 0])
+(defn digit? [str]
+  (Character/isDigit (first str)))
 
-(into [] (take 2 '(1 0)))
+(defn find-match [acc number-array]
+  (if (= 0 (count number-array))
+    [acc]
+    (apply concat
+           (for [r (range 1 (inc (count number-array)))]
+             (let [[curr next] (split-at r number-array)
+                   fs (if (= (count curr) 1)
+                        [['(1) (str (nth curr 0))]]
+                        (mapped-dictionary curr))]
+               (apply concat
+                      (for [[_ f] fs]
+                        (if (and f
+                                 (or (= (count acc) 0)
+                                     (not (and (digit? f)
+                                               (digit? (last acc))))))
+                          (find-match (conj acc f) next)))))))))
+
+(defn format-res [res]
+  (map #(clojure.string/join " " %) res))
+
+(defn strings-for-number [str-number]
+  (->> str-number
+       to-number-array
+       (find-match [])
+       format-res
+       (map (fn [x] [str-number x]))))
+
+(defn do-prog []
+  (with-open [rdr (clojure.java.io/reader (clojure.java.io/resource "input.txt"))]
+    (let [rows (line-seq rdr)]
+      (doseq [number rows]
+        (doseq [[n r] (strings-for-number number)]
+          (println n ": " r))))))
 
 (defn -main
-  "I don't do a whole lot ... yet."
   [& args]
-  (println "Hello, World!"))
+  (do-prog))
